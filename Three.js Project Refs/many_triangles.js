8c5f03aa4aa1d4bd4020a8a05afe46ad0f3506d9
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-10-13 18:33:39 -0500

    Update and rename many_triangles.mp4 to many_triangles.js

---
+++ Three.js Project Refs/many_triangles.js
@@ -0,0 +1,89 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+
+const geometry = new THREE.BufferGeometry()
+
+const count = 50 // # of triangles
+const positionsArray = new Float32Array(count * 3 * 3) // triangles times 3 vertices times 3 coordinates
+
+for(let i = 0; i < count * 3 * 3; i++) // random triangles
+{
+    positionsArray[i] = (Math.random() - 0.5) * 4  // create random coordinates for vertices. * 4 makes it bigger.
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3) // pass in the coordinates
+geometry.setAttribute('position', positionsAttribute) // must use postion for now
+
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xff0000,
+    wireframe: true
+})
+
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
